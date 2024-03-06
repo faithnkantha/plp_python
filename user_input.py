ea1877919187024fc980f019315687a4f8b339eb
Faith nkatha <faithnkatha160@gmail.com>
2024-03-06 10:40:38 +0300

    Delete user_input.py

--- user_input.py
+++
@@ -1,4 +0,0 @@
-name=input("enter your name: ")
-age=input("Enter your age:")
-location=input("Enter your Location:")
-print("Hello {},you are {} years old and live in {}".format(name,age,location))
